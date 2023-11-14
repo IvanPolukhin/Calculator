c6e0dcc784f239fcec6a004a111f0c86b552ea43
IvanPolukhin <ivan.poluhin.200407@gmail.com>
2023-11-14 19:44:50 +0200

    end of job

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,22 @@
 function data(value) {
+    clearOnFirstDigit();
     document.getElementById('display').value += value;
 }
 
 function result() {
     let expression = document.getElementById('display').value;
     let result;
-    result = /^[0-9+\-*/.\s]+$/.test(expression) ? eval(expression) : 'Error';
+    result = /^[0-9+\-*/.\s]+$/.test(expression) ? eval(expression) : '0';
     document.getElementById('display').value = result;
 }
 
+function clearOnFirstDigit() {
+    let currentValue = document.getElementById('display').value;
+    if (currentValue === '0') {
+        document.getElementById('display').value = '';
+    }
+}
+
 function clearDisplay() {
-    document.getElementById('display').value = '';
+    document.getElementById('display').value = '0';
 }
